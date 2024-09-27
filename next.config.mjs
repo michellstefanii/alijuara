63f23c56e320a45b3dc9c6d0e03c90d3ba72dc80
Michel Santos Stefani <michell.stefani@msn.com>
2024-09-26 22:01:55 -0300

    refactor: add project

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "i.ibb.co",
       },
+    {
+      protocol: "",
+      hostname: "gnvA8aVm6RIE@ep-damp-darkness-a5pgprje.us-east-2.aws.neon.tech"
+    }
     ],
   },
 };
