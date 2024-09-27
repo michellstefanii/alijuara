8ca488b7ea4b24bbc0e9116c89c0fd45fa9cacba
Michel Santos Stefani <michell.stefani@msn.com>
2024-09-26 22:26:23 -0300

    refactor: add project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
-
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "500", "700", "900"],
