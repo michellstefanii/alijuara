173e90c0b07525c926e4a63b5896d7a4d26757c1
Michel Santos Stefani <michell.stefani@msn.com>
2024-09-26 22:04:02 -0300

    refactor: add project

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "i.ibb.co",
       },
-    {
-      protocol: "",
-      hostname: "gnvA8aVm6RIE@ep-damp-darkness-a5pgprje.us-east-2.aws.neon.tech"
-    }
     ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
   },
 };
 
