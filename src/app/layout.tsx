d9fa5a0223536af2c7fd3596837474e792e7f283
Michel Santos Stefani <michell.stefani@msn.com>
2024-09-28 21:14:40 -0300

    refactor: add pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "500", "700", "900"],
@@ -18,6 +19,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1101174108009670');
+              fbq('track', 'PageView');
+            `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1101174108009670&ev=PageView&noscript=1"
+        />
+      </noscript>
       <body className={roboto.className}>{children}</body>
     </html>
   );
